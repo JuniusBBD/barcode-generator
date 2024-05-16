1d86d0b77d30f487e77052347f4b50fd7d989625
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2024-05-16 11:15:49 +0200

    add value as a dependency

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -42,7 +42,7 @@ export function Card(props: CardProps) {
       .catch((err) => {
         console.log(err);
       });
-  }, [ref]);
+  }, [ref, value]);
 
   React.useEffect(() => {
     handleGenerateBarcode();
